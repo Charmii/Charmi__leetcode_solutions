b4793479c30f0eb42c503280ec5cd91017831cb9
Charmi Shah <charmishah1030@gmail.com>
2023-08-20 17:14:58 +0530

    Time: 2 ms (43.85%), Space: 6.2 MB (27.62%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        if(a == b) return -1;
+        else if(a.length() == b.length()) return a.length();
+        else{
+            return max(a.length(),b.length());
+        }
+    }
+};
