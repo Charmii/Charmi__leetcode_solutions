8ff9ec940e892cba133e78ae67074da70789f368
Charmi Shah <charmishah1030@gmail.com>
2023-08-26 18:57:23 +0530

    Time: 11 ms (5.51%), Space: 6.6 MB (34.68%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     void findCombinations(int index, int k, int target, vector<vector<int>>& ans, vector<int>& ds) {
-        if(ds.size() == k ){
+        cout<<ds.size()<<" "<<index<<" "<<target<<" "<<k<<endl;
+        for(int i=0;i<ds.size();i++){
+            cout<<ds[i];
+        }
+        cout<<endl;
+        if(index > 10) return;
+
+        if(ds.size() == k || ds.size() == 9){
+            cout<<"first id";
             if(target == 0){
+                cout<<"pushing";
                 ans.push_back(ds);
             }
             return;
         }
 
-        for (int i = index; i <= 9; i++) {
-            ds.push_back(i);
-            findCombinations(i+1, k, target - i, ans, ds);
+            ds.push_back(index);
+            findCombinations(index+1, k, target - index, ans, ds);
             ds.pop_back();
-        }
+            findCombinations(index+1, k, target, ans, ds);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
