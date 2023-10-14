fe959549a8f11461e4ac64b1d6d9446cbbb3ad16
Charmi Shah <charmishah1030@gmail.com>
2023-10-14 13:47:14 +0530

    Time: 140 ms (5.4%), Space: 44.9 MB (16.2%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
@@ -15,6 +15,7 @@ public:
         vector<vector<int>> adj(n,vector<int>());
         vector<int> indegree(n,0);
         vector<int> outdegree(n,0);
+        int root=-1, countroots=0;
 
         for (int i = 0; i < n; i++) {
             adj[i].push_back(leftChild[i]);
@@ -35,7 +36,7 @@ public:
                     return false;}
             }
         }
-        int root=-1, countroots=0;
+        
         for(int i=0; i<n; i++){
             if(indegree[i] == 0){
                 root = i;
