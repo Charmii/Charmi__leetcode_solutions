22d86f8ef4b473dbed5692270b17c7c15a1f900d
Charmi Shah <charmishah1030@gmail.com>
2023-11-02 14:15:36 +0530

    Time: 127 ms (69.99%), Space: 72.8 MB (90.11%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if(nums.size() <= 1) return false;
+        unordered_set<int> mp;
+        for(int i=0; i < nums.size(); i++){
+            if(i > k){
+                mp.erase(nums[i-k-1]);
+            }
+            if(mp.find(nums[i]) != mp.end()) return true;
+            mp.insert(nums[i]);
+        }
+        return false;
+    }
+};
